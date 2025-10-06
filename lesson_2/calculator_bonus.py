428674b74b8f28ec16c870c86178b93080d1b480
Jacob Morrin <jacob.morrin@gmail.com>
2025-10-06 08:56:25 -0400

    new languages added

--- lesson_2/calculator_bonus.py
+++ lesson_2/calculator_bonus.py
@@ -3,6 +3,11 @@
 # Ask the user for an operation to perform.
 # Perform the operation on the two numbers.
 # Print the result to the terminal.
+LANGUAGE = 'zh'
+import json
+
+with open('calculator_messages.json', 'r') as file:
+    MESSAGES = json.load(file)
 
 def prompt(message):
     print(f'==> {message}')
@@ -14,29 +19,31 @@ def invalid_number(number_str):
         return True
     return False
 
-prompt('Welcome to Calculator!')
+def get_message(message, LANGUAGE):
+    return MESSAGES[LANGUAGE][message]
 
-while True: 
-    prompt("What's the first number?")
+prompt(get_message('welcome', LANGUAGE))
+
+while True:
+    prompt(get_message('first_number', LANGUAGE))
     number1 = input()
 
     while invalid_number(number1):
-        prompt("Hmm... that doesn't look like a valid number.")
+        prompt(get_message('invalid_number', LANGUAGE))
         number1 = input()
 
-    prompt("What's the second number?")
+    prompt(get_message('second_number', LANGUAGE))
     number2 = input()
 
     while invalid_number(number2):
-        prompt("Hmm... that doesn't look like a valid number.")
+        prompt(get_message('invalid_number', LANGUAGE))
         number2 = input()
 
-    prompt("""What operation would you like to perform
-    1) Add 2) Subtract 3) Multiply 4) Divide""")
+    prompt(get_message('choose_operation', LANGUAGE))
     operation = input()
 
     while operation not in ['1', '2', '3', '4']:
-        prompt('You must choose 1, 2, 3, or 4')
+        prompt(get_message('reject_operation', LANGUAGE))
         operation = input()
 
     match operation:
@@ -49,21 +56,14 @@ while True:
         case '4':
             output = int(number1) / int(number2)
 
-    prompt(f'The result is: {output}')
-    
+    prompt(f'{get_message('result', LANGUAGE)} {output}')
+
     # Ask user if they would like to perform another operation
-    prompt("""Would you like to perform another operation?
-    1) Yes 2) No""")
+    prompt(get_message('another_operation', LANGUAGE))
     answer = input()
     while answer not in ['1', '2']:
-        prompt('You must choose 1 or 2')
+        prompt(get_message('invalid_choice', LANGUAGE))
         answer = input()
-    
-    if answer == '1':
-        continue
-    else:
+
+    if answer != '1':
         break
-
-
-    
-
