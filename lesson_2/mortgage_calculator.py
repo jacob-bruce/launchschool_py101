2473ea8113f5c9ecaa81f4f11fe82c546273f35d
Jacob Morrin <jacob.morrin@gmail.com>
2025-10-06 14:31:39 -0400

    refactored helper functions

--- lesson_2/mortgage_calculator.py
+++ lesson_2/mortgage_calculator.py
@@ -39,6 +39,70 @@ def invalid_positive_number(number_str):
 
     return False
 
+def get_loan_amount():
+    prompt('what_is_loan_amount')
+    loan_amount = input()
+
+    while invalid_negative_number(loan_amount):
+        prompt('invalid_number')
+        loan_amount = input()
+
+    loan_amount = float(loan_amount)
+    return loan_amount
+
+def get_interest_rate():
+    prompt('what_is_annual_rate')
+    monthly_rate = input()
+
+    while invalid_positive_number(monthly_rate):
+        prompt('invalid_number')
+        monthly_rate = input()
+
+    monthly_rate = float(monthly_rate) / 12
+    return monthly_rate
+
+def get_loan_duration():
+    prompt('what_is_loan_duration')
+    monthly_loan_duration = input()
+
+    while invalid_negative_number(monthly_loan_duration):
+        prompt('invalid_number')
+        monthly_loan_duration = input()
+
+    monthly_loan_duration = float(monthly_loan_duration)
+    return monthly_loan_duration
+
+def calculate_payment(amount, rate, duration):
+    if rate > 0:
+        monthly_payment = amount * (
+            rate /
+            (1 - (1 + rate) ** (-duration))
+        )
+    else:
+        monthly_payment = amount / duration
+    return monthly_payment
+
+def print_result(loan_duration, monthly_payment):
+    start_date = date.today()
+    payoff_date = (
+        start_date
+        + relativedelta(months =+int(loan_duration))
+        )
+    print(
+        f"{MORTGAGE_MESSAGES[USER_LANGUAGE]['your_payment_amount']}"
+        f"${monthly_payment:,.2f}.\n"
+        f"{MORTGAGE_MESSAGES[USER_LANGUAGE]['your_payoff_date']} "
+        f"{payoff_date.strftime('%B %Y')}\n"
+    )
+
+def perform_another_calculation():
+    prompt('another_operation')
+    answer = input()
+    while answer not in ['1', '2']:
+        prompt('invalid_choice')
+        answer = input()
+    return answer
+
 # Select language
 print("""==> Select language
         1) English 
@@ -48,8 +112,8 @@ print("""==> Select language
 language_choice = input()
 
 while language_choice not in ['1', '2', '3']:
-    prompt('invalid_language')
-    language_choice = input()
+    print(MORTGAGE_MESSAGES[DEFAULT_LANGUAGE]['invalid_language'])
+    language_choice = input("==> ")
 
 match language_choice:
     case '1':
@@ -62,65 +126,18 @@ match language_choice:
 # Welcome the user
 prompt('welcome')
 
-# Ask the user for the loan amount and convert to float
+# Main loop
 while True:
-    prompt('what_is_loan_amount')
-    loan_amount = input()
-
-    while invalid_negative_number(loan_amount):
-        prompt('invalid_number')
-        loan_amount = input()
-
-    loan_amount = float(loan_amount)
-
-# Ask the user for the APR and convert to float
-    prompt('what_is_annual_rate')
-    monthly_rate = input()
-
-    while invalid_positive_number(monthly_rate):
-        prompt('invalid_number')
-        monthly_rate = input()
-
-    monthly_rate = float(monthly_rate) / 12
-
-# Ask the user for the loan duration and convert to float
-    prompt('what_is_loan_duration')
-    monthly_loan_duration = input()
-
-    while invalid_negative_number(monthly_loan_duration):
-        prompt('invalid_number')
-        monthly_loan_duration = input()
-
-    monthly_loan_duration = float(monthly_loan_duration)
-
-# Calculate the monthly payment
-    if monthly_rate > 0:
-        monthly_payment = loan_amount * (
-            monthly_rate /
-            (1 - (1 + monthly_rate) ** (-monthly_loan_duration))
+    user_loan_amount = get_loan_amount()
+    user_monthly_rate = get_interest_rate()
+    user_monthly_loan_duration = get_loan_duration()
+    user_monthly_payment = calculate_payment(
+        user_loan_amount,
+        user_monthly_rate,
+        user_monthly_loan_duration
         )
-    else:
-        monthly_payment = loan_amount / monthly_loan_duration
-
 # Print monthly payment
-    start_date = date.today()
-    payoff_date = (
-        start_date
-        + relativedelta(months =+int (monthly_loan_duration))
-        )
-    print(
-        f"{MORTGAGE_MESSAGES[USER_LANGUAGE]['your_payment_amount']}"
-        f"${monthly_payment:,.2f}.\n"
-        f"{MORTGAGE_MESSAGES[USER_LANGUAGE]['your_payoff_date']} "
-        f"{payoff_date.strftime('%B %Y')}\n"
-    )
-
+    print_result(user_monthly_loan_duration, user_monthly_payment)
 # Ask user if they would like to perform another operation
-    prompt('another_operation')
-    answer = input()
-    while answer not in ['1', '2']:
-        prompt('invalid_choice')
-        answer = input()
-
-    if answer != '1':
+    if perform_another_calculation() !=  '1':
         break
